3b340406f2caa81336e6b62f4d8f78db57621fcf
mvpotter <supermegapotter@gmail.com>
2014-06-20 16:27:10 +0700

    Issue with file names is fixed

--- parsley-fields-comparison-validators.js
+++ parsley-fields-comparison-validators.js
@@ -1,31 +1,27 @@
 // Greater than validator
 window.ParsleyValidator.addValidator('gt', 
 	function (value, requirement) {
-    	var smaller_number = $(requirement).val();
-        return value > smaller_number;
+        return parseFloat(value) > parseFloat($(requirement).val());
     }, 32)
     .addMessage('en', 'gt', 'This value should be greater');
 
 // Greater than or equal to validator
 window.ParsleyValidator.addValidator('ge', 
 	function (value, requirement) {
-    	var smaller_number = $(requirement).val();
-        return value >= smaller_number;
+        return parseFloat(value) >= parseFloat((requirement).val());
     }, 32)
     .addMessage('en', 'ge', 'This value should be greater or equal');
 
 // Less than validator
 window.ParsleyValidator.addValidator('lt', 
 	function (value, requirement) {
-    	var bigger_number = $(requirement).val();
-        return value < bigger_number;
+        return parseFloat(value) < parseFloat((requirement).val());
     }, 32)
     .addMessage('en', 'lt', 'This value should be less');
 
 // Less than or equal to validator
 window.ParsleyValidator.addValidator('le', 
 	function (value, requirement) {
-    	var bigger_number = $(requirement).val();
-        return value <= bigger_number;
+        return parseFloat(value) <= parseFloat((requirement).val());
     }, 32)
     .addMessage('en', 'le', 'This value should be less or equal');
