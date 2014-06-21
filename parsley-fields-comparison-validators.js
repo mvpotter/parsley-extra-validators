67668708c7055337ee253d4494f34ec40dd16847
mvpotter <supermegapotter@gmail.com>
2014-06-21 14:12:05 +0700

    notequalto validator is added

--- parsley-fields-comparison-validators.js
+++ parsley-fields-comparison-validators.js
@@ -1,3 +1,10 @@
+//  Not equal to validator
+window.ParsleyValidator.addValidator('notequalto', 
+    function (value, requirement) {
+        return value !== $(requirement).val();
+    }, 32)
+    .addMessage('en', 'notequalto', 'This value should not be the same.');
+
 // Greater than validator
 window.ParsleyValidator.addValidator('gt', 
 	function (value, requirement) {
